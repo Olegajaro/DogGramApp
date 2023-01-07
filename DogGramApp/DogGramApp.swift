6a1ef56706ac90e1eb84ce82ac22873f45280942
Oleg Fedorov <olegajaro94@gmail.com>
2023-01-07 11:23:16 +0700

    Connect app to Firebase, update gitignore

--- DogGramApp/DogGramApp.swift
+++ DogGramApp/DogGramApp.swift
@@ -5,10 +5,16 @@
 //  Created by Олег Федоров on 01.01.2023.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct DogGramApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
